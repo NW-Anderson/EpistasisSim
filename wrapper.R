a1a760e97bce894495e5a735f7989a30f6178c20
Nathan <nathan.wally.anderson@gmail.com>
2021-07-14 23:36:10 -0500

    ..

--- wrapper.R
+++ wrapper.R
@@ -1,25 +1,26 @@
 library(doMC)
 library(stringr)
 library(foreach)
-opts <- list(preschedule = FALSE)
-registerDoMC(5)
+library(EasyABC)
+# opts <- list(preschedule = FALSE)
+# registerDoMC(5)
 #################
 ## ABC wrapper ##
 #################
 ## test params ##
 #################
-seed = 1
-npops = 10
-nloci = 1156
-RR = 0.5
-popsize = 100
-fmin = 0
-fmax = 1
-s = 1
-r = -0.05
-a = 0.2
-b = -130
-par <- c(seed, npops, nloci, RR, popsize, fmin, fmax, s, r, a, b)
+# seed = 1
+# npops = 10
+# nloci = 1156
+# RR = 0.5
+# popsize = 100
+# fmin = 0
+# fmax = 1
+# s = 1
+# r = -0.05
+# a = 0.2
+# b = -130
+# par <- c(seed, npops, nloci, RR, popsize, fmin, fmax, s, r, a, b)
 ####################
 ## model function ##
 ####################
@@ -53,31 +54,39 @@ model <- function(par){
                "_s=", s,
                "_r=", r,
                "_a=", a,
-               "_b=", b, ".csv", sep = ""), intern = T)
+               "_b=", b, ".csv", sep = ""))
   
   rawout <- as.matrix(read.csv(file = paste('./output/', list.files(path = './output')[1], sep = '')))[,-1159]
-  sigsnps6 <- vector("list",10)
-  names(sigsnps6) <- paste('pop', 1:10)
-  for(pop in 1:10){
-    popdat <- rawout[which(rawout[,2] == pop),]
-    sigsnps6[[pop]] <- which((popdat[2,3:ncol(popdat)] - popdat[1,3:ncol(popdat)]) >= 0.01)
-  }
-  jaccmat <- array(dim = c(10,10))
-  for(j in 1:9){
-    for(k in (j+1):10){
-      jaccmat[j,k] <- length(intersect(sigsnps6[[j]], sigsnps6[[k]])) / length(union(sigsnps6[[j]], sigsnps6[[k]]))
+  results <- vector(length = 10)
+  for(i in 0:1){
+    signsnps <- vector("list",10)
+    names(signsnps) <- paste('pop', 1:10)
+    for(pop in 1:10){
+      popdat <- rawout[which(rawout[,2] == pop),]
+      if(i == 0){
+        signsnps[[pop]] <- which((popdat[3,3:ncol(popdat)] - popdat[1,3:ncol(popdat)]) >= 0.01)
+      }else if(i == 1){
+        signsnps[[pop]] <- which((popdat[3,3:ncol(popdat)] - popdat[1,3:ncol(popdat)]) >= 0.01)
+      }
     }
+    jaccmat <- array(dim = c(10,10))
+    for(j in 1:9){
+      for(k in (j+1):10){
+        jaccmat[j,k] <- length(intersect(signsnps[[j]], signsnps[[k]])) / length(union(signsnps[[j]], signsnps[[k]]))
+      }
+    }
+    jaccmat <- na.omit(as.vector(jaccmat))
+    results[(5 * i + 1):(5 *  i + 5)] <- quantile(jaccmat)
   }
-  jaccmat <- na.omit(as.vector(jaccmat))
   system(paste("rm ./output/", list.files(path = './output')[1], sep = ''))
-  return(jaccmat)
+  return(results)
 }
 ###########
 ## prior ##
 ###########
 #par <- c(seed, npops, nloci, RR, popsize, fmin, fmax, s, r, a, b)
 
-prior <- list(c("unif",10,11), # npops 
+prior <- list(c("unif",10,10), # npops 
               c("unif",1156,1156), # nloci
               c("unif",0.5,0.5), # RR
               c("unif",2000,2000), # popsize
@@ -91,14 +100,21 @@ prior <- list(c("unif",10,11), # npops
 ##############
 ## Observed ##
 ##############
-observed <- readRDS("jaccard.empirical.AFC_01.RDS")
+rawout <- readRDS("jaccard.empirical.AFC_01.RDS")
+observed <- vector(length = 10)
+for(i in 0:1){
+  observed[(5 * i + 1):(5 * i + 5)] <- quantile(rawout[[i+1]])
+}
+rm(rawout, i)
+##################
+## Call easyABC ##
+##################
+# setwd("~/Documents/GitHub/EpistasisSim")
+setwd(("/media/lee/HDD_Array/nwanderson/EpistasisSim"))
+ABC_SLiM <- ABC_sequential(method="Lenormand", use_seed=TRUE,
+                           model=model, prior=prior, summary_stat_target=observed,
+                           nb_simul=5, n_cluster = 8) 
+
+save(ABC_SLiM, file = "ABCoutput.RData")
 
 
-
-
-
-
-
-################
-## Deprecated ##
-################
