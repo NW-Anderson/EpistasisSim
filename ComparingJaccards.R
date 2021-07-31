b6e041c24017c9be8f1fcdc6003a981cbd363e21
Nathan <nathan.wally.anderson@gmail.com>
2021-07-31 17:36:51 -0500

    Started making violin plots

--- ComparingJaccards.R
+++ ComparingJaccards.R
@@ -1,12 +1,12 @@
 setwd("~/Documents/GitHub/EpistasisSim")
 
-iter <- 2
+iter <- 1
 
-seeds <- sample(1:2^15, 2 * iter)
+seeds <- sample(1:2^15, 4 * iter)
 npops = 10
 nloci = 1156
-RR = 0.5
-popsize = 100
+RR = 0.49
+popsize = 2000
 fmin = 0
 fmax = 1
 
@@ -14,11 +14,14 @@ fmax = 1
 ###########################
 ## Directional Epistasis ##
 ###########################
-load("~/Documents/GitHub/EpistasisSim/DirectionalEpistasis.RData")
-
-shat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,1])
-rhat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,2])
-bhat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,3]) * 100
+# load("~/Documents/GitHub/EpistasisSim/DirectionalEpistasis.RData")
+# 
+# shat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,1])
+# rhat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,2])
+# bhat <- sum(ABC_SLiM$weights * ABC_SLiM$param[,3]) * 100
+shat <- 1
+rhat <- -10
+bhat <- -0.4
 
 DirectionalJaccards <- list("Gen6" = c(),
                             "Gen10" = c())
@@ -75,10 +78,13 @@ for(i in 1:iter){
 ###########################
 ## Exponential Epistasis ##
 ###########################
-load("~/Documents/GitHub/EpistasisSim/ExponentialEpistasis.RData")
+# load("~/Documents/GitHub/EpistasisSim/ExponentialEpistasis.RData")
+# 
+# ahat <- sum(Exponential$weights * Exponential$param[,1])
+# bhat <- sum(Exponential$weights * Exponential$param[,2]) * 100
 
-ahat <- sum(Exponential$weights * Exponential$param[,1])
-bhat <- sum(Exponential$weights * Exponential$param[,2]) * 100
+ahat <- 5
+bhat <- -1
 
 ExponentialJaccards <- list("Gen6" = c(),
                             "Gen10" = c())
@@ -128,6 +134,61 @@ for(i in 1:iter){
                "_b=", bhat, ".csv", sep = ''))
 }
 
+###################################
+## Diminishing Returns Epistasis ##
+###################################
+
+ahat <- 10
+bhat <- -0.4
+
+DimRetJaccards <- list("Gen6" = c(),
+                            "Gen10" = c())
+for(i in 1:iter){
+  system(paste("slim -d seed=", seeds[2 * iter + i],
+               " -d npops=", npops,
+               " -d nloci=", nloci,
+               " -d RR=",RR,
+               " -d popsize=", popsize,
+               " -d " ,'"', 'fitnessFunction=', "'", 'diminishingReturns', "'", '"',
+               " -d fmin=", fmin,
+               " -d fmax=", fmax,
+               " -d b=", bhat,
+               " -d a=", ahat,
+               " EpistaticSim.slim | tail -n +14 > output/diminishingReturns",
+               "_seed=", seeds[2 * iter + i],
+               "_a=", ahat,
+               "_b=", bhat, ".csv", sep = ""))
+  rawout <- as.matrix(read.csv(file = paste('./output/diminishingReturns',
+                                            "_seed=", seeds[2 * iter + i],
+                                            "_a=", ahat,
+                                            "_b=", bhat, ".csv", sep = "")))[,-1159]
+  for(gen in 1:2){
+    signsnps <- vector("list",10)
+    names(signsnps) <- paste('pop', 1:10)
+    for(pop in 1:10){
+      popdat <- rawout[which(rawout[,2] == pop),]
+      if(gen == 1){
+        signsnps[[pop]] <- which((popdat[2,3:ncol(popdat)] - popdat[1,3:ncol(popdat)]) >= 0.01)
+      }else if(gen == 2){
+        signsnps[[pop]] <- which((popdat[3,3:ncol(popdat)] - popdat[1,3:ncol(popdat)]) >= 0.01)
+      }
+    }
+    jaccmat <- array(dim = c(10,10))
+    for(j in 1:9){
+      for(k in (j+1):10){
+        jaccmat[j,k] <- length(intersect(signsnps[[j]], signsnps[[k]])) / length(union(signsnps[[j]], signsnps[[k]]))
+      }
+    }
+    jaccmat <- na.omit(as.vector(jaccmat))
+    # results[(5 * i + 1):(5 *  i + 5)] <- quantile(jaccmat)
+    DimRetJaccards[[gen]] <- c(DimRetJaccards[[gen]], jaccmat)
+  }
+  system(paste("rm ./output/diminishingReturns",
+               "_seed=", seeds[2 * iter + i],
+               "_a=", ahat,
+               "_b=", bhat, ".csv", sep = ''))
+}
+
 #####################
 ## Selective Sweep ##
 #####################
@@ -135,7 +196,12 @@ SSJaccards <- list("Gen6" = c(),
                    "Gen10" = c())
 
 for(i in 1:iter){
-  system(paste("slim Sim.slim | tail -n +14 > output/polygenic.csv", sep = ""))
+  system(paste("slim -d seed=", seeds[3 * iter + i],
+               " -d npops=", npops,
+               " -d nloci=", nloci,
+               " -d RR=",RR,
+               " -d popsize=", popsize,
+  " Sim.slim | tail -n +14 > output/polygenic.csv", sep = ""))
   rawout <- as.matrix(read.csv(file = paste('./output/polygenic.csv', sep = "")))[,-1159]
   for(gen in 1:2){
     signsnps <- vector("list",10)
@@ -159,3 +225,46 @@ for(i in 1:iter){
     SSJaccards[[gen]] <- c(SSJaccards[[gen]], jaccmat)
   }
 }
+
+##############
+## Plotting ##
+##############
+library(vioplot)
+treatment <- c(rep("Diminishing Returns", times = 90), 
+               rep("Directional", times = 90), 
+               rep("Exponential", times = 90), 
+               rep("Selective Sweep", times = 90))
+
+generation <- c(rep(c(6,10), times = 4, each = 45))
+Jaccards <- c(DimRetJaccards$Gen6, 
+              DimRetJaccards$Gen10,
+              DirectionalJaccards$Gen6,
+              DirectionalJaccards$Gen10,
+              ExponentialJaccards$Gen6,
+              ExponentialJaccards$Gen10,
+              SSJaccards$Gen6,
+              SSJaccards$Gen10)
+data <- data.frame(treatment, generation, Jaccards)
+
+ggraptR(data)
+
+####################
+## Finished Plots ##
+####################
+
+ggplot(data, aes(y=Jaccards, x=as.factor(treatment))) + 
+  geom_boxplot(aes(fill=as.factor(generation)), 
+               stat="boxplot", position="dodge", alpha=0.5, width=0.2) + 
+  geom_violin(aes(fill=as.factor(generation)), stat="ydensity", 
+              position="dodge", alpha=0.5, trim=TRUE, scale="area") + 
+  theme_grey() + 
+  theme(text=element_text(family="sans", face="plain", color="#000000", 
+                          size=15, hjust=0.5, vjust=0.5)) + 
+  guides(fill=guide_legend(title="generation")) + 
+  ggtitle("1156 Loci: No Linkage") + 
+  xlab("Treatment") + 
+  ylab("Jaccard Score")
+
+
+
+
