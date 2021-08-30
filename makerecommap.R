a5a7283e72e41787a4d8beefa96c500af94bf94b
Nathan <nathan.wally.anderson@gmail.com>
2021-08-30 15:48:03 -0500

    ..
    corrected mistake with characters instead of integers in sortedsnpdata.csv
    
    got mutdrift to run in gui
    
    moved parsed jaccard out of ssd

--- makerecommap.R
+++ makerecommap.R
@@ -5,7 +5,13 @@ y <- readRDS("hap_block_snps.neutral_AFC_cutoffs.RDS")
 
 tmpdata <- cbind(x$chr, x$pos, x$haplotype_block, y$T0, x$selCoef, x$cov, y$Gen10_neutAFC99, y$Gen10_neutAFC999)
 tmpdata <- data.frame(tmpdata)
-colnames(tmpdata) <- c("chr", 'pos', 'haplotype_block', 'y$T0', 'selCoef', 'cov', 'Gen10_neutAFC99', 'Gen10_neutAFC999')
+colnames(tmpdata) <- c("chr", 'pos', 'haplotype_block', 'T0', 'selCoef', 'cov', 'Gen10_neutAFC99', 'Gen10_neutAFC999')
+tmpdata$pos <- as.integer(tmpdata$pos)
+tmpdata$T0 <- as.numeric(tmpdata$T0)
+tmpdata$selCoef <- as.numeric(tmpdata$selCoef)
+tmpdata$cov <- as.integer(tmpdata$cov)
+tmpdata$Gen10_neutAFC99 <- as.numeric(tmpdata$Gen10_neutAFC99)
+tmpdata$Gen10_neutAFC999 <- as.numeric(tmpdata$Gen10_neutAFC999)
 sorteddata <- c()
 for(haps in unique(tmpdata$haplotype_block)){
   hapdata <- tmpdata[which(tmpdata$haplotype_block == haps),]
